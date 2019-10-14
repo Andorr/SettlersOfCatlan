bb32e6d35fb37a0cddbc56b296827ea971be540e
Halvor Fladsrud Bø <halvor@highered.global>
2019-10-14 16:15:31 +0200

    added basic state classes

---
+++ Assets/_Scripts/State/Map.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map 
+{
+    public Player[] players;
+    public Location[] locations;
+    public Tile[] tiles;
+    public Path[] paths;
+}
