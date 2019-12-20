a09fe13784e642806d28966934f5ab8aacedf22d
WilliamCJacobsen <willia2210@hotmail.no>
2019-12-20 20:21:32 +0100

    removed .anim folres for sheeps and added a sheepController

---
+++ Assets/_Scripts/Logic/SheepController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SheepController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 0.4f;
+    public float walkTime = 4f;
+    public GameObject[] waypoints;
+    public int waypointRadius = 1;
+    private int randSpot;
+    private Vector3 newPos;
+    void Start()
+    {
+        randSpot = Random.Range(0,waypoints.Length);
+        Debug.Log(randSpot);
+        newPos = waypoints[randSpot].transform.position;
+        Debug.Log(newPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float dist = Vector3.Distance(transform.position, newPos);
+
+        if(dist < waypointRadius){
+            randSpot = Random.Range(0,waypoints.Length);
+            newPos = waypoints[randSpot].transform.position;
+        }
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, newPos, step);
+
+    }
+
+
+}
