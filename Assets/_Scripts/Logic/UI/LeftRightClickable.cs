ad8583587c71f3bd3b35792adf110bd3cba57bf7
Anders Hallem Iversen <andershallemiversen@hotmail.com>
2019-12-23 04:04:54 +0100

    Started to work on UI for player trading

---
+++ Assets/_Scripts/Logic/UI/LeftRightClickable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class LeftRightClickable : MonoBehaviour, IPointerClickHandler
+{
+    public UnityEvent onLeft;
+    public UnityEvent onRight;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left) {
+            onLeft.Invoke();
+        } else if(eventData.button == PointerEventData.InputButton.Right) {
+            onRight.Invoke();
+        }
+    }
+}
